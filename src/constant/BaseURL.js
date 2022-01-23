ce6e4b7ba421100e9ed48de404e78660d201e5c1
sumesh <sumeshkumarkuchya@gmail.com>
2022-01-24 01:50:46 +0530

    changed baseURL

--- src/constant/BaseURL.js
+++ src/constant/BaseURL.js
@@ -1,7 +1,7 @@
-export const baseURL_Seller_Service = "http://seller-service-env.eba-2kbtwhiu.us-east-1.elasticbeanstalk.com"
-export const baseURL_Buyer_Service = "http://buyer-service-env.eba-2kbtwhiu.us-east-1.elasticbeanstalk.com"
-export const baseURL_Inventory_Service = "http://inventory-service.eba-2kbtwhiu.us-east-1.elasticbeanstalk.com"
-export const baseURL_Payment_Order_Service = "http://payment-and-order-service-env.eba-2kbtwhiu.us-east-1.elasticbeanstalk.com"
+export const baseURL_Seller_Service = "https://main.d3g3zwqnci6ko4.amplifyapp.com/http://seller-service-env.eba-2kbtwhiu.us-east-1.elasticbeanstalk.com"
+export const baseURL_Buyer_Service = "https://main.d3g3zwqnci6ko4.amplifyapp.com/http://buyer-service-env.eba-2kbtwhiu.us-east-1.elasticbeanstalk.com"
+export const baseURL_Inventory_Service = "https://main.d3g3zwqnci6ko4.amplifyapp.com/http://inventory-service.eba-2kbtwhiu.us-east-1.elasticbeanstalk.com"
+export const baseURL_Payment_Order_Service = "https://main.d3g3zwqnci6ko4.amplifyapp.com/http://payment-and-order-service-env.eba-2kbtwhiu.us-east-1.elasticbeanstalk.com"
 
 
 // export const baseURL_Seller_Service = "http://localhost:9001"
