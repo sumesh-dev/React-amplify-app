12960038bcff011f11a6103e122193420f2ad678
sumesh <sumeshkumarkuchya@gmail.com>
2022-01-24 01:13:46 +0530

    changed baseURL

--- src/constant/BaseURL.js
+++ src/constant/BaseURL.js
@@ -1,10 +1,10 @@
+export const baseURL_Seller_Service = "http://seller-service-env.eba-2kbtwhiu.us-east-1.elasticbeanstalk.com"
+export const baseURL_Buyer_Service = "http://buyer-service-env.eba-2kbtwhiu.us-east-1.elasticbeanstalk.com"
+export const baseURL_Inventory_Service = "http://inventory-service.eba-2kbtwhiu.us-east-1.elasticbeanstalk.com"
+export const baseURL_Payment_Order_Service = "http://payment-and-order-service-env.eba-2kbtwhiu.us-east-1.elasticbeanstalk.com"
+
+
 // export const baseURL_Seller_Service = "http://localhost:9001"
 // export const baseURL_Buyer_Service = "http://localhost:9002"
 // export const baseURL_Inventory_Service = "http://localhost:9003"
 // export const baseURL_Payment_Order_Service = "http://localhost:9005"
-
-
-export const baseURL_Seller_Service = "http://localhost:9001"
-export const baseURL_Buyer_Service = "http://localhost:9002"
-export const baseURL_Inventory_Service = "http://localhost:9003"
-export const baseURL_Payment_Order_Service = "http://localhost:9005"
